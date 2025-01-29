410ce92812b7e55bbd2bbd0b64e34a996758b7d9
janvi-08 <janvi1105.becse24@chitkara.edu.in>
2025-01-29 23:57:54 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -5,7 +5,7 @@ int main(){
     if(ch=='a'||ch=='e'|| ch=='i' || ch=='o'||ch=='u'){
         printf("Vowel");
     }
-    else if(ch>='b'&&ch<='z') && (ch!='a'|| ch!='e' || ch!='i' || ch!='o' || ch!='u'){
+    else if(ch>='b'&&ch<='z'){
         printf("Consonant");
     }
     else if(ch>='0' && ch<='9'){

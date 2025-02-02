2de3ce5f06122a6d6b05d8df22930028a8b1f87b
janvi-08 <janvi1105.becse24@chitkara.edu.in>
2025-02-02 15:17:11 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -2,14 +2,13 @@
 int main(){
 int a,b; 
 char ch;
-scanf("%d%d",&a,&b);
-scanf("%c",&ch);
+scanf("%d %d %c",&a,&b,&ch);
 if(ch=='+'){printf("%d",a+b);}
 else if(ch=='-'){printf("%d",a-b);}
 else if(ch=='*'){printf("%d",a*b);}
 else if(ch=='/'){
     if(b==0){printf("error");
-    }
+    }else if(a%b==0){printf("%d",a/b);}
  }
  else{printf("error");}
 }

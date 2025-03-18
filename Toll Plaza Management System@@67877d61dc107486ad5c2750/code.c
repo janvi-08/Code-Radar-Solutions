4f535e7577dab6030f7ba0d305653df853217c67
Janvi Mittal <janvimittal0805@gmail.com>
2025-03-18 16:36:50 +0530

    Solution to Toll Plaza Management System

--- Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -8,7 +8,7 @@ struct vehicle{
 int main(){
     int n;
     scanf("%d",&n);
-    struct Employee s[n];
+    struct vehicle s[n];
     for(int i=0;i<n;i++){
         scanf("%s",s[i].num);
         scanf("%s",s[i].type);

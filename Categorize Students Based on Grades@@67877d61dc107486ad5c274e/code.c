983307f53c34d8865609d13295d734e71d9f64ae
Janvi Mittal <janvimittal0805@gmail.com>
2025-03-19 17:57:01 +0530

    Solution to Categorize Students Based on Grades

--- Categorize Students Based on Grades@@67877d61dc107486ad5c274e/code.c
+++ Categorize Students Based on Grades@@67877d61dc107486ad5c274e/code.c
@@ -17,7 +17,7 @@ int main(){
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             if(s[i+1].marks>s[i].marks){
-                struct student.temp=s[i];
+                struct student temp=s[i];
                 s[i]=s[i+1];
                 s[i+1]=temp;
             }

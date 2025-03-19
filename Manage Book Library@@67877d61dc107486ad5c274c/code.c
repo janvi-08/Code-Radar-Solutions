da5b207fdd4f8bfc0384506fcc310b33b825e96a
Janvi Mittal <janvimittal0805@gmail.com>
2025-03-19 17:19:18 +0530

    Solution to Manage Book Library

--- Manage Book Library@@67877d61dc107486ad5c274c/code.c
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -12,7 +12,7 @@ int main(){
     for(int i=0;i<n;i++){
         scanf("%s",s[i].book);
         scanf("%s",s[i].Author);
-        scanf("%f",&s[i].price);
+        scanf("%d",&s[i].price);
     }
     int k;
     scanf("%d",&k);

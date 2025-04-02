71702cb5b5d2640137bb4ec87fc74f51c71f6d7c
Janvi Mittal <janvimittal0805@gmail.com>
2025-04-02 23:39:05 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

---
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -0,0 +1,26 @@
+int duplicates(int ranked[],int n){
+    if (n == 0) return 0; 
+    int unique=1;
+    for(int i=1;i<n;i++){
+        if(ranked[i]!=ranked[i-1]){
+            ranked[unique++]=ranked[i];
+        }
+    }
+    return unique;
+}
+
+void trackPlayerRanks(int ranked[],int n,int player[],int m,int result[] ){
+    int size=duplicates(int ranked[],int n){
+        for(int j=0;j<m;j++){
+            int playerscore=player[j];
+            int rank =size + 1;
+        }
+        for(int k=0;k<size;k++){
+            if(playerscore>=ranked[k]){
+                rank=k+1;
+                break;
+            }
+        }
+         result[j] = rank;
+    }
+}

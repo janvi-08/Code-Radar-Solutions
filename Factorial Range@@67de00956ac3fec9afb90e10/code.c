dfe8dc7feb665776ff58bcc0de24ee6aa09f4e71
Janvi Mittal <janvimittal0805@gmail.com>
2025-04-02 22:24:10 +0530

    Solution to Factorial Range

--- Factorial Range@@67de00956ac3fec9afb90e10/code.c
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -2,7 +2,7 @@ int factorialRange(int start,int end){
     int product=1;
     for(int i=start;i<=end;i++){
         if(i==1 || i==0){
-            int fact=-1;
+            int fact=1;
             printf("%d\n",fact);
         }
         else{

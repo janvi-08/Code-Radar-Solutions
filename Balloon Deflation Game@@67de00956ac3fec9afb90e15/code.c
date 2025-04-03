a8128295a471a1ada80a00a18f67304460bffdb0
Janvi Mittal <janvimittal0805@gmail.com>
2025-04-04 00:04:30 +0530

    Solution to Balloon Deflation Game

--- Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
+++ Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
@@ -1,30 +1,49 @@
-#include<limits.h>
-int deflateBalloons(int air[],int n){
+#include <stdio.h>
+#include <limits.h>
+
+int deflateBalloons(int air[], int n) {
     int balloons[n];
     for (int i = 0; i < n; i++) {
         balloons[i] = air[i];
     }
-    while(1){
-           int min=INT_MAX;
-        for(int i=0;i<n;i++){
-            if(balloons[i]<min){
-                min=balloons[i];
+    
+    int count = 0;
+    
+
+    while (1) {
+        int min = INT_MAX; 
+        
+        
+        for (int i = 0; i < n; i++) {
+            if (balloons[i] > 0 && balloons[i] < min) {
+                min = balloons[i];
             }
         }
-    
-    if(min==INT_MAX){
-        break;
-    }
-    for(int i=0;i<n;i++){
-        balloons[i]=balloons[i]-min;
-    }
-    
-    int count=0;
-    for(int i = 0; i < n; i++) {
-            if(balloons[i] > 0) {
-                count++;
+        
+        
+        if (min == INT_MAX) {
+            break;
+        }
+        
+        
+        for (int i = 0; i < n; i++) {
+            if (balloons[i] > 0) {
+                balloons[i] -= min;
             }
         }
-        printf("%d/n",count);
+        
+       
+        int currentCount = 0;
+        for (int i = 0; i < n; i++) {
+            if (balloons[i] > 0) {
+                currentCount++;
+            }
+        }
+        
+        
+        printf("%d\n", currentCount);
     }
+    
+    return 0; 
 }
+

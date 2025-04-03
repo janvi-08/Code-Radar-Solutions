60fb97510ba4d74d2a69e0a5d3b749f68cf0cb41
Janvi Mittal <janvimittal0805@gmail.com>
2025-04-04 00:06:37 +0530

    Solution to Balloon Deflation Game

--- Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
+++ Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
@@ -3,36 +3,35 @@
 
 int deflateBalloons(int air[], int n) {
     int balloons[n];
+    
+    // Copy the air values into the new array 'balloons'
     for (int i = 0; i < n; i++) {
         balloons[i] = air[i];
     }
     
-    int count = 0;
-    
-
     while (1) {
-        int min = INT_MAX; 
+        int min = INT_MAX; // Initialize min to the highest possible value
         
-        
+        // Find the smallest non-zero air amount
         for (int i = 0; i < n; i++) {
             if (balloons[i] > 0 && balloons[i] < min) {
                 min = balloons[i];
             }
         }
         
-        
+        // If no balloon has air left, break the loop
         if (min == INT_MAX) {
             break;
         }
         
-        
+        // Subtract the minimum value from all balloons with air left
         for (int i = 0; i < n; i++) {
             if (balloons[i] > 0) {
                 balloons[i] -= min;
             }
         }
         
-       
+        // Count how many balloons still have air
         int currentCount = 0;
         for (int i = 0; i < n; i++) {
             if (balloons[i] > 0) {
@@ -40,10 +39,26 @@ int deflateBalloons(int air[], int n) {
             }
         }
         
-        
+        // Print the count of balloons that still have air at the start of this round
         printf("%d\n", currentCount);
     }
     
-    return 0; 
+    return 0; // No need to return anything for this task
 }
 
+int main() {
+    int n;
+    scanf("%d", &n); // Read the number of balloons
+    
+    int air[n]; // Array to store the initial air values of the balloons
+    
+    // Read the air values of the balloons
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &air[i]);
+    }
+    
+    // Call the function to process the balloon deflation
+    deflateBalloons(air, n);
+    
+    return 0;
+}

9268d27c05b96e8c9864b46b4f84628db5140049
Janvi Mittal <janvimittal0805@gmail.com>
2025-04-08 02:32:41 +0530

    Solution to Calculate Average Temperature of a Week

---
+++ Calculate Average Temperature of a Week@@67877d61dc107486ad5c274d/code.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+struct Day{
+    char name[100];
+    float temp;
+};
+struct week{
+    struct Day days[7];
+};
+int main(){
+    struct week s;
+    char *daynames[]={"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    float sum=0;
+    for(int i=0;i<7;i++){
+        strcpy(s.days[i],*daynames[i]);
+        scanf("%d",s.days[i].temp);
+        sum += s.days[i].temp;
+    }
+    float avg=sum/7.0;
+    printf("Average Temperature: %.2f",avg);
+    }
+     

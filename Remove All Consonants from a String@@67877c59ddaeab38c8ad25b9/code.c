ac9847e5284126f2fa7fc7845fc3fda893175525
Janvi Mittal <janvimittal0805@gmail.com>
2025-04-08 01:51:27 +0530

    Solution to Remove All Consonants from a String

--- Remove All Consonants from a String@@67877c59ddaeab38c8ad25b9/code.c
+++ Remove All Consonants from a String@@67877c59ddaeab38c8ad25b9/code.c
@@ -3,23 +3,19 @@
 
 int main() {
     char ch[100];
-    char result[100]; // To store vowels
+    char result[100]; 
     int j = 0;
 
-    scanf("%[^\n]s", ch);
+    scanf("%[^\n]", ch);
 
-    // Loop through the string to extract vowels
     for (int i = 0; ch[i] != '\0'; i++) {
         if (ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'i' || ch[i] == 'o' || ch[i] == 'u' || 
             ch[i] == 'A' || ch[i] == 'E' || ch[i] == 'I' || ch[i] == 'O' || ch[i] == 'U') {
-            result[j++] = ch[i]; // Add vowel to result string
+            result[j++] = ch[i]; 
         }
     }
-
-    result[j] = '\0'; // Null-terminate the result string
-
-    // Print the modified string
-    printf(result);
+    result[j] = '\0'; 
+    printf("%s",result);
 
     return 0;
 }
